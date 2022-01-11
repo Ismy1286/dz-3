f85a57ab0ecdd9fc172c9d232ca82e9997a3e571
Ainur <ismy1286@gmail.com>
2022-01-11 13:06:07 +0600

    first commit

---
+++ DZ-4.5.3/CustonButton.swift
@@ -0,0 +1,8 @@
+//
+//  CustonButton.swift
+//  DZ-4.5.3
+//
+//  Created by user on 10.01.2022.
+//
+
+import Foundation
---
+++ DZ-4.5.3/NameCell.swift
@@ -0,0 +1,8 @@
+//
+//  NameCell.swift
+//  DZ-4.5.3
+//
+//  Created by user on 10.01.2022.
+//
+
+import Foundation
